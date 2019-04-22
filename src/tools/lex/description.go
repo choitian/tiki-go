ada784cba4c7bae1eaf89fecb370939bcd19d980
choitian <choitian@163.com>
2019-04-22 14:16:32 +0800

    4.22

--- src/tools/lex/description.go
+++ src/tools/lex/description.go
@@ -1,70 +1,16 @@
 package lex
 
 import (
-	"bufio"
-	"os"
-	"regexp"
-	"strconv"
-	"strings"
+	"encoding/xml"
 )
 
-type Line struct {
-	Pos    string
-	Text   string
-	Script string
+type Tokens struct {
+	XMLName xml.Name `xml:"tokens"`
+	Tokens  []Token  `xml:"token"`
 }
 
-type Description struct {
-	Lines []Line
-}
-
-func readLines(path string) ([]string, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	var lines []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	return lines, scanner.Err()
-}
-func reNamedMap(r *regexp.Regexp, str string) map[string]string {
-	match := r.FindStringSubmatch(str)
-	if match == nil {
-		return nil
-	}
-	subMatchMap := make(map[string]string)
-	for i, name := range r.SubexpNames() {
-		if i != 0 && name != "" {
-			subMatchMap[name] = match[i]
-		}
-	}
-
-	return subMatchMap
-}
-func NewDescription(path string) (*Description, error) {
-	des := new(Description)
-
-	lines, err := readLines(path)
-	if err != nil {
-		return nil, err
-	}
-
-	var myExp = regexp.MustCompile(`^(?P<text>.+)(?P<script>\{.+\})`)
-	for i, v := range lines {
-		ret := reNamedMap(myExp, v)
-		if ret != nil {
-			var line Line
-			line.Pos = strconv.Itoa(i)
-			line.Text = strings.TrimSpace(ret["text"])
-			line.Script = strings.TrimSpace(ret["script"])
-
-			des.Lines = append(des.Lines, line)
-		}
-	}
-	return des, nil
+type Token struct {
+	XMLName xml.Name `xml:"token"`
+	Tok     string   `xml:"tok,attr"`
+	Regx    string   `xml:"regx"`
 }
---
+++ src/tools/lex/lexutil/lexutil.go
@@ -0,0 +1,38 @@
+package lexutil
+
+import (
+	"bufio"
+	"encoding/xml"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func LeadLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+func RegxNamedMap(r *regexp.Regexp, str string) map[string]string {
+	match := r.FindStringSubmatch(str)
+	if match == nil {
+		return nil
+	}
+	subMatchMap := make(map[string]string)
+	for i, name := range r.SubexpNames() {
+		if i != 0 && name != "" {
+			subMatchMap[name] = match[i]
+		}
+	}
+	return subMatchMap
+}
